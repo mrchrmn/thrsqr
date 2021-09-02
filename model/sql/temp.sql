899c7cbd67eddaa5a8b6f2b08a7dfb991e5e8a4c
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-09-02 21:32:05 +0200

    minor changes

---
+++ model/sql/temp.sql
@@ -0,0 +1,14 @@
+ALTER TABLE events
+ALTER COLUMN id TYPE varchar(8);
+
+ALTER TABLE participants
+ALTER COLUMN id TYPE varchar(8);
+
+ALTER TABLE responses
+ALTER COLUMN event_id TYPE varchar(8);
+
+ALTER TABLE responses
+ALTER COLUMN participant_id TYPE varchar(8);
+
+ALTER TABLE admins
+ALTER COLUMN username TYPE varchar(16);
